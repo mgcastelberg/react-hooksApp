1ce38fc50a539f411abdf029382f82d2117d0fae
Manuel Gomez <jmgc@virket.com>
2025-01-12 00:16:57 -0600

    useCounter con CustomHook y Exponer métodos del Hook

---
+++ src/hooks/useCounter.js
@@ -0,0 +1,28 @@
+// Hook no es mas que una funcion que se puede reutilizar en cualquier componente
+
+import { useState } from "react"
+
+export const useCounter = (initialValue=10) => {
+
+    const [counter, setCounter] = useState( initialValue );
+
+    const increment = ( value = 1 ) => {
+        setCounter( counter + value );
+    }
+
+    const decrement = ( value = 1 ) => {
+        if ( counter === 0 ) return;
+        setCounter( counter - value );
+    }
+
+    const reset = () => {
+        setCounter( initialValue );
+    }
+
+    return {
+        counter,
+        increment,
+        decrement,
+        reset,
+    }
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HooksApp } from './HooksApp.jsx';
 import './index.css';
-import { CounterApp } from './01-useState/CounterApp.jsx';
+// import { CounterApp } from './01-useState/CounterApp.jsx';
+import { CounterWhitCustomHook } from './01-useState/CounterWhitCustomHook.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HooksApp />
-    <CounterApp />
+    {/* <CounterApp /> */}
+    { <CounterWhitCustomHook /> }
   </React.StrictMode>,
 )
